38279624edc143ebd8069331a40c64b3ecd1464c
Byte-Wh1te <bytewh1te@yahoo.com>
2023-12-27 17:52:53 +0200

    header guard for calamity.h

--- include/Calamity/Calamity.h
+++ include/Calamity/Calamity.h
@@ -1,37 +1,39 @@
-    #pragma once
+#ifndef CALAMITY_H
+#define CALAMITY_H
 
-    #include "Renderer.h"
-    #include "Vertex.h"
-    #include "Shader.h"
-    #include "Window.h"
-    #include "Texture.h"
-    #include "Framebuffer.h"
-    #include <glm/glm.hpp>
-    #include <vector>
-
-
-
-    namespace clm{
-    // TODO: Make it useful
-    class Calamity
-    {
-    Window& window;
-    Shader& shader;
-    VertexArray va;
-    IndexBuffer ib;
-    Renderer renderer;
+#include "Renderer.h"
+#include "Vertex.h"
+#include "Shader.h"
+#include "Window.h"
+#include "Texture.h"
+#include "Framebuffer.h"
+#include <glm/glm.hpp>
+#include <vector>
 
 
-    std::vector<float> vertexData;
-    std::vector<GLuint> indicesData;
-    public: 
-        Calamity(Window& window,Shader& shader);
-        ~Calamity();
-        void Render();
-        void DrawRect(float x,float y,float w,float h);
-        void DrawTestTriangle();
 
-        void UpdateData();
-    };
+namespace clm{
+// TODO: Make it useful
+class Calamity
+{
+Window& window;
+Shader& shader;
+VertexArray va;
+IndexBuffer ib;
+Renderer renderer;
 
-    }// namespace clm
+
+std::vector<float> vertexData;
+std::vector<GLuint> indicesData;
+public: 
+    Calamity(Window& window,Shader& shader);
+    ~Calamity();
+    void Render();
+    void DrawRect(float x,float y,float w,float h);
+    void DrawTestTriangle();
+
+    void UpdateData();
+};
+
+}// namespace clm
+#endif // CALAMITY_H
