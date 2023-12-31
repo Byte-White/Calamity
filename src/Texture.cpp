d1791473431527b75365ede425dd73bd19ce8d8b
Byte-Wh1te <bytewh1te@yahoo.com>
2023-12-31 23:56:09 +0200

    repeat instead of clamp to edge

--- src/Texture.cpp
+++ src/Texture.cpp
@@ -19,8 +19,8 @@ namespace clm
         glBindTexture(GL_TEXTURE_2D, m_RendererID);
 
         // Set texture parameters for wrapping and filtering
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 
